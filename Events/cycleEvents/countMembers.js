a0a8dc29224d9084a576a2e59995f38d5e78cc99
Guilherme <guilherme.nono45789@gmail.com>
2021-09-01 18:13:06 -0300

    Update countMembers.js

--- Events/cycleEvents/countMembers.js
+++ Events/cycleEvents/countMembers.js
@@ -2,7 +2,7 @@ module.exports = async (client) => {
     const guild = client.guilds.cache.get('466405222877495299')
     setInterval(() => {
         const memberCount = guild.memberCount
-        const channel = guild.channels.cache.get('842314262847815680')
-        channel.setName(`| Membros ➟ ${memberCount}|`)
+        const channel = guild.channels.cache.get('882734568204021801')
+        channel.setName(`👥┃Membros ━ᐅ ${memberCount}`)
     }, 10000);
 }
